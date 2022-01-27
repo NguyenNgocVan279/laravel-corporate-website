dc19f613f59e04ef6bd28eadce7fbdb28f362162
NguyenNgocVan279 <nguyenngocvan279@gmail.com>
2022-01-27 23:11:06 +0700

    Frontend Master Layout

---
+++ resources/views/frontend/body/left_area.blade.php
@@ -0,0 +1,28 @@
+<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+<aside id="colorlib-aside" role="complementary" class="border js-fullheight">
+    <h1 id="colorlib-logo"><a href="index.html">VinaSOFT</a></h1>
+    <nav id="colorlib-main-menu" role="navigation">
+        <ul>
+            <li class="colorlib-active"><a href="index.html">Home</a></li>
+            <li><a href="services.html">Dịch vụ</a></li>
+            <li><a href="work.html">Dự án</a></li>
+            <li><a href="blog.html">Chia sẻ</a></li>
+            <li><a href="contact.html">Liên hệ</a></li>
+        </ul>
+    </nav>
+
+    <div class="colorlib-footer">
+        <p>
+            <small>&copy; <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> Made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://vinasoft.net" target="_blank">VinaSoft</a> Distributed by: <a href="https://vinasoft.net/" target="_blank">VinaSoft.Net</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> </span> <span>Demo Images: <a href="https://vinasoft.net/" target="_blank">vinasoft.net</a></span></small>
+        </p>
+        <ul>
+            <li><a href="#"><i class="icon-facebook2"></i></a></li>
+            <li><a href="#"><i class="icon-twitter2"></i></a></li>
+            <li><a href="#"><i class="icon-instagram"></i></a></li>
+            <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+        </ul>
+    </div>
+
+</aside>
